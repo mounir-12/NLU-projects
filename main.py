5965674b51ea7d6dd47746410fb404b4f018544f
Ali H <sonorligh@gmail.com>
2019-04-21 22:28:48 +0200

    Extended and improved pipeline, grad. clipping - Now reading train and eval data - ...

--- main.py
+++ main.py
@@ -18,47 +18,61 @@ HIDDEN_SIZE = 512
 HIDDEN_SIZE = 128  # For local dev
 N_LINES = None  # Number of training lines to read
 N_LINES = 20  # For local dev
+MAX_GRAD_NORM = 5
+
+
+def tokenize(path, lang, max_len, nlines=None):
+    all_tokens = []
+
+    with open(path) as in_file:
+        for i, line in enumerate(in_file):
+            tokens = lang.tokenize(line)
+            if len(tokens) < max_len:
+                all_tokens.append(tokens)
+
+            if nlines and i == nlines:
+                break
+
+    return all_tokens
+
+
+def build_lang(lang, all_tokens):
+    for tokens in all_tokens:
+        lang.add_tokens(tokens)
+
+    lang.build()
+
+
+def get_dataset(lang, all_tokens):
+    all_embedded = [None] * len(all_tokens)
+    all_targets = [None] * len(all_tokens)
+    for i, tokens in enumerate(all_tokens):
+        in_tokens = lang.numericalize(tokens, MAX_LEN)
+        out_tokens = list(in_tokens)
+
+        in_tokens.insert(0, lang.BOS_token)
+        out_tokens.append(lang.EOS_token)  # Input shifted in time by one and having EOS
+
+        all_embedded[i] = in_tokens
+        all_targets[i] = out_tokens
+
+    return tf.data.Dataset.from_tensor_slices((all_embedded, all_targets)).batch(BATCH_SIZE)
+
 
 lang = Lang()
+train_tokens = tokenize(os.path.join('data', 'sentences.train'), lang, MAX_LEN, N_LINES)
+eval_tokens = tokenize(os.path.join('data', 'sentences.eval'), lang, MAX_LEN, N_LINES)
 
-all_tokens = []
+build_lang(lang, train_tokens)
 
-with open(os.path.join('data', 'sentences.train')) as in_file:
-    i = 0
-    for i, l in enumerate(in_file):
-        tokens = lang.tokenize(l)
-        if len(tokens) < MAX_LEN:
-            lang.add_tokens(tokens)
-            all_tokens.append(tokens)
+train_ds = get_dataset(lang, train_tokens)
+eval_ds = get_dataset(lang, eval_tokens)
 
-        if N_LINES and i == N_LINES:
-            break
-
-lang.build()
-
-all_embedded = [None] * len(all_tokens)
-all_targets = [None] * len(all_tokens)
-
-for i, tokens in enumerate(all_tokens):
-    in_tokens = lang.numericalize(tokens, MAX_LEN)
-    out_tokens = list(in_tokens)
-
-    in_tokens.insert(0, lang.BOS_token)
-    out_tokens.append(lang.EOS_token)  # Input shifted in time by one and having EOS
-
-    all_embedded[i] = in_tokens
-    all_targets[i] = out_tokens
-
-
-# print("\n".join(str(e) for e in all_tokens))
-# print("\n".join(str(e) for e in all_embedded))
-
-train = tf.data.Dataset.from_tensor_slices((all_embedded, all_targets)).batch(BATCH_SIZE)
-
-print(train.output_shapes)
+print(train_ds.output_shapes)
 
 
 def compute_loss(logits, labels):
+    # with tf.GradientTape() as tape:
     return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
 
 
@@ -66,6 +80,14 @@ model = LSTM(vocab_size=len(lang), embed_size=EMBEDDING_SIZE, hidden_size=HIDDEN
 
 optimizer = tf.train.AdamOptimizer()
 
-for (batch, (input, labels)) in enumerate(train):
-    optimizer.minimize(lambda: compute_loss(logits=model(input), labels=labels))
 
+def clip_gradients(grads_and_vars, clip_ratio):
+    grads, vars = zip(*grads_and_vars)
+    clipped, _ = tf.clip_by_global_norm(grads, clip_ratio)
+    return zip(clipped, vars)
+
+
+for (batch, (input, labels)) in enumerate(train_ds):
+    # grads_and_vars = optimizer.compute_gradients(lambda: compute_loss(logits=model(input), labels=labels))
+    grads = tfe.implicit_gradients(compute_loss)
+    optimizer.apply_gradients(clip_gradients(grads(model(input), labels), MAX_GRAD_NORM))
--- model.py
+++ model.py
@@ -12,23 +12,35 @@ class LSTM:
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
 
-        self.weights = tf.Variable(initializer((hidden_size, vocab_size)))
-        self.biases = tf.Variable(initializer([vocab_size]))
-        self.embed_matrix = tf.Variable(initializer((vocab_size, embed_size)))
+        self.weights = tf.Variable(initializer((hidden_size, vocab_size)), name="weights")
+        self.biases = tf.Variable(initializer([vocab_size]), name="biases")
+        self.embed_matrix = tf.Variable(initializer((vocab_size, embed_size)), name="embed")
 
         self.rnn = LSTMCell(hidden_size, initializer=initializer)
+
+        self.trainable_variables = [self.weights, self.biases, self.embed_matrix, self.rnn]
 
     def forward(self, x):
         embedded = tf.nn.embedding_lookup(self.embed_matrix, x)
 
-        logits = tf.Variable(tf.zeros((embedded.shape[0], embedded.shape[1], self.vocab_size)))  # TODO Initialize with empty tensor
-        states = (tf.zeros((embedded.shape[0], self.hidden_size)), tf.zeros((embedded.shape[0], self.hidden_size)))  # TODO Is this the right initialization for the hidden state ?
+        states = self.rnn.zero_state(embedded.shape[0], tf.float32)
 
+        """
+        embed_seq = tf.unstack(embedded, axis=1)
+        logits = []
+        for el in embed_seq:
+            output, states = self.rnn(el, states)
+            logits.append(tf.matmul(output, self.weights) + self.biases)
+
+        logits = tf.stack(logits, axis=1)
+        """
+
+        logits = tf.Variable(
+            tf.zeros((embedded.shape[0], embedded.shape[1], self.vocab_size)))  # TODO Initialize with empty tensor
         for i in range(embedded.shape[1]):  # TODO Use tf.while_loop for compatibility with graph mode ?
             output, states = self.rnn(embedded[:, i, :], states)
             logits[:, i, :].assign(tf.matmul(output, self.weights) + self.biases)
 
-        # TODO Use the last output instead ?
         return logits
 
     def __call__(self, x):
