9201cafedb1a1b3b63e20419e240e519fac372dc
Ali H <sonorligh@gmail.com>
2019-04-21 17:23:55 +0200

    WIP Main pipeline, tokenization and embedding

--- lang.py
+++ lang.py
@@ -8,18 +8,18 @@ class Lang:
 
         self._built = False
 
-        self.UNK_token = 0
+        self.PAD_token = 0
         self.BOS_token = 1
         self.EOS_token = 2
-        self.PAD_token = 3
+        self.UNK_token = 3
 
         self.token2count = Counter()
         self.token2index = defaultdict(lambda: self.UNK_token)
         self.index2token = {
-            self.UNK_token: '<unk>',
+            self.PAD_token: '<pad>',
             self.BOS_token: '<bos>',
             self.EOS_token: '<eos>',
-            self.PAD_token: '<pad>'
+            self.UNK_token: '<unk>'
         }
         self.n_tokens = len(self.index2token)  # Count UNK, SOS, EOS and PAD
 
@@ -49,8 +49,26 @@ class Lang:
 
         self._built = True
 
+    def embed(self, tokens, length=30):
+        assert len(tokens) - 2 < length
+        embedded = [self.token2index[token] for token in tokens]
+        self.pad_tokens(embedded, length - 2)  # Take into account BOS and EOS tokens
+
+        embedded.insert(0, self.BOS_token)
+        embedded.append(self.EOS_token)
+
+        assert len(embedded) == length
+        assert embedded[0] == self.BOS_token
+        assert embedded[-1] == self.EOS_token
+
+        return embedded
+
+    def pad_tokens(self, tokens: list, to_length=30):
+        tokens.extend([self.PAD_token] * (to_length - len(tokens)))
+        return tokens
+
     def pad(self, sentence: str, to_length=30):
-        length = sentence.count(' ') + 1
+        length = sentence.strip().count(' ') + 1
         return sentence + (' %s' % self.index2token[self.PAD_token]) * (to_length - length)
 
     def dump(self, path: str):
--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 import os
 
 import tensorflow as tf
+import tensorflow.contrib.eager as tfe
 
 from lang import Lang
 
+tfe.enable_eager_execution()
+tf.set_random_seed(9)
+
+MAX_LEN = 28  # 30 - 2 (BOS and EOS)
+BATCH_SIZE = 64
+N_LINES = 20  # Number of training lines to read
+
 lang = Lang()
+
+all_tokens = []
 
 with open(os.path.join('data', 'sentences.train')) as in_file:
     i = 0
-    for l in in_file:
-        lang.add_sentence(l)
+    for i, l in enumerate(in_file):
+        tokens = lang.tokenize(l)
+        if len(tokens) < MAX_LEN:
+            lang.add_tokens(tokens)
+            all_tokens.append(tokens)
+
+        if N_LINES and i == N_LINES:
+            break
 
 lang.build()
+
+all_embedded = [None] * len(all_tokens)
+
+for i, tokens in enumerate(all_tokens):
+    all_embedded[i] = lang.embed(tokens)
+
+
+# print("\n".join(str(e) for e in all_tokens))
+# print("\n".join(str(e) for e in all_embedded))
+
+train = tf.data.Dataset.from_tensor_slices(all_embedded).batch(BATCH_SIZE)
+
+print(train.output_shapes)
 
 hidden_size = 512
 
