33ce90c568e88f946eb06122e0a1608dce1a0e5f
Ali H <sonorligh@gmail.com>
2019-05-25 17:19:15 +0200

    Project 2
    - data processing
    - initial baseline model
    - remove ultra big model files of project 1 (couldn't push because of them)

---
+++ project1/main.py
@@ -0,0 +1,295 @@
+import datetime
+import os
+import sys
+from argparse import ArgumentParser
+from time import time
+
+import math
+import numpy as np
+import tensorflow as tf
+
+from lang import Vocabulary, Corpus
+from model import LSTM
+
+parser = ArgumentParser()
+parser.add_argument("-l", "--lines", type=int,
+                    help="Number of lines to read. If not provided, reads the whole file")
+
+parser.add_argument("-bs", "--batch-size", type=int, default=64,
+                    help="Train batch size")
+parser.add_argument("-e", "--epochs", type=int, default=1,
+                    help="Training epochs")
+
+parser.add_argument("-pe", "--print-every", type=int, default=100,
+                    help="Number of steps between info printing")
+parser.add_argument("-ve", "--val-every", type=int, default=1000,
+                    help="Number of steps between evaluation metric computation")
+parser.add_argument("-se", "--save-every", type=int, default=1000,
+                    help="Number of steps between model checkpoint saving")
+
+parser.add_argument('-t', "--task", choices=["1a", "1b", "1c", "2"], required=True,
+                    help="The task to run")
+
+parser.add_argument("-rs", "--random-seed", type=int, default=9,
+                    help="Random seed")
+
+args = parser.parse_args()
+
+tf.set_random_seed(args.random_seed)
+np.random.seed(args.random_seed)
+
+# --------------------------------------------------------CONSTANTS---------------------------------------------------------------------
+# ------values in comment for cluster deployement-------
+batch_size = args.batch_size
+num_epochs = args.epochs
+eval_every = args.val_every
+print_every = args.print_every  # limit the number of prints during training
+save_every = args.save_every
+n_lines = args.lines
+print("Variables: batch_size: {}, num_epochs: {}, eval_every: {}, print_every: {}, save_every: {}, n_lines: {}".format(
+    batch_size, num_epochs, eval_every, print_every, save_every, n_lines))
+max_length = 20  # maximum number of words per sentence during completion
+# ------------------------------------------------------
+task = args.task
+
+train_path = os.path.join(os.getcwd(), "data", "sentences.train")
+eval_path = os.path.join(os.getcwd(), "data", "sentences.eval")
+test_path = os.path.join(os.getcwd(), "data", "sentences_test.txt")
+embedding_path = os.path.join(os.getcwd(), "data", "wordembeddings-dim100.word2vec")
+prompt_path = os.path.join(os.getcwd(), "data", "sentences.continuation")
+
+sentence_len = 30  # padded sentence length including EOS and BOS
+vocab_size = 20000  # vocabulary size
+clip_grad_norm = 5
+model_task2 = "modelC.ckpt"
+
+
+# ------------------------------------------------------FUNCTIONS---------------------------------------------------------------------
+
+def get_data(corpus, shuffle=False, batch_size=None):
+    data = corpus.data
+    data_size = data.shape[0]
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        data = data[shuffle_indices]
+    x = data[:, :-1]
+    y = data[:, 1:]
+    num_batches = 1  # default value
+    if batch_size is not None:
+        num_batches = int(math.ceil(data_size / batch_size))
+    x = np.array_split(x, num_batches)  # split into num_batches batches (last batch size may differ from previous ones)
+    y = np.array_split(y, num_batches)  # split into num_batches batches (last batch size may differ from previous ones)
+    return np.array(x), np.array(y)
+
+
+# Trains the model and returns perplexity values on the eval sentences
+def train_model(model, sess, num_epochs, train_x_batched, train_y_batched, eval_x_batched, eval_y_batched,
+                model_ckpt_name="model.ckpt", eval_every=-1):
+    # Training loop
+    models_dir = os.path.join(os.getcwd(), "models")
+    model_path = os.path.join(models_dir, model_ckpt_name)  # path of file to save model
+    num_batches_train = train_x_batched.shape[0]  # nb of batches in the training set
+    num_batches_eval = eval_x_batched.shape[0]  # nb of batches in the eval set
+
+    # train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+    if os.path.exists(models_dir) and model.load_model(sess, model_path):  # if successfully loaded model
+        step, step_loss = eval_model(model, sess, eval_x_batched, eval_y_batched, num_batches_eval)
+        perps = get_perplexity(model, sess, eval_x_batched, eval_y_batched,
+                               V_train)  # compute perplexities over eval dataset
+        mean, median = np.mean(perps), np.median(perps)
+        print("\nEvaluating restored model on eval dataset:"
+              "\n   batches: {}, step {}, loss {}, perp_mean: {}, perp_median {}\n".format(num_batches_eval, step,
+                                                                                           step_loss, mean, median))
+        return
+
+    # otherwise, train model and save
+    print("Training model ...")
+    # Initialize all variables
+    sess.run(tf.global_variables_initializer())
+    for e in range(num_epochs):
+        for b in range(num_batches_train):
+            _, step, step_loss = model.train_step(sess, train_x_batched[b], train_y_batched[b])
+            if step == 1 or step % print_every == 0:
+                time_str = datetime.datetime.now().isoformat()
+                print("epoch {}, batch {}:\n{}: step {}, loss {}".format(e + 1, b + 1, time_str, step, step_loss))
+            if step % save_every == 0:
+                model.save_model(sess, model_path)  # save trained model
+            if eval_every > 0 and step % eval_every == 0:  # do not evaluate if eval_every <= 0
+                perps = get_perplexity(model, sess, eval_x_batched, eval_y_batched,
+                                       V_train)  # compute perplexities over eval dataset
+                mean, median = np.mean(perps), np.median(perps)
+                print("\nEvaluation:\n    batches: {}, step: {}, perp_mean: {}, perp_median: {}\n".format(
+                    num_batches_eval, step, mean, median))
+
+    model.save_model(sess, model_path)  # save trained model
+
+
+def eval_model(model, sess, eval_x_batched, eval_y_batched, num_batches):
+    sum = 0
+    counter = 0
+    for i in range(num_batches):  # compute the loss over each batch
+        step, step_loss = model.eval_step(sess, eval_x_batched[i], eval_y_batched[i])
+        if not np.isnan(step_loss):
+            sum += step_loss
+            counter += 1
+    step_loss = sum / counter  # compute the mean loss over all batches
+    return step, step_loss
+
+
+def get_perplexity(model, sess, test_x_batched, test_y_batched, V_train):
+    num_batches = test_x_batched.shape[0]
+    perp = None
+    for i in range(num_batches):
+        batch_perp = model.perplexity(sess, test_x_batched[i], test_y_batched[i], V_train)
+        if perp is None:
+            perp = batch_perp
+        else:
+            perp = np.concatenate((perp, batch_perp))
+    return perp
+
+
+def write_out(array, file_name):
+    """
+    Writes the given array in a file with the given name inside the output folder.
+    If 'filename' already exists, it will be overwritten.
+
+    :param array: data to write
+    :param file_name: name of the file
+    """
+    n = array.shape[0]
+    with open(os.path.join('output', file_name), 'w') as output:
+        for row in range(n):  # write each row
+            output.write(str(array[row]) + '\n')
+
+
+# --------------------------------------------------------START---------------------------------------------------------------------
+# Data IO
+print("\nData IO ...")
+t = time()
+C_train = Corpus(train_path, sentence_len, read_from_file="corpus_train.pkl",
+                 n_sentences=n_lines)  # create training corpus from training file
+V_train = Vocabulary(vocab_size, read_from_file="vocab.pkl")  # object to represent training vocabulary
+V_train.build_from_corpus(C_train, write_to_file="vocab.pkl")  # build vocabulary from training corpus
+C_train.build_data_from_vocabulary(V_train,
+                                   write_to_file="corpus_train.pkl")  # build corpus data matrix from vocabulary, write object to disk
+
+C_eval = Corpus(eval_path, sentence_len, read_from_file="corpus_eval.pkl",
+                n_sentences=n_lines)  # create evaluation corpus from evaluation file
+C_eval.build_data_from_vocabulary(V_train,
+                                  write_to_file="corpus_eval.pkl")  # build corpus data matrix from vocabulary, write object to disk
+
+C_test = Corpus(test_path, sentence_len, read_from_file="corpus_test.pkl",
+                n_sentences=n_lines)  # create test corpus from test file
+C_test.build_data_from_vocabulary(V_train,
+                                  write_to_file="corpus_test.pkl")  # build corpus data matrix from vocabulary, write object to disk
+
+print("Total time (s): ", time() - t, "\n")
+
+print("Vocab size: ", V_train.vocab_size)
+print("Train data matrix shape: ", C_train.data.shape)
+print("Eval data matrix shape: ", C_eval.data.shape)
+
+# Train and Eval data
+train_x_batched, train_y_batched = get_data(C_train, shuffle=True,
+                                            batch_size=batch_size)  # training data, shuffled, batched
+# eval and test data batched but not shuffled
+eval_x_batched, eval_y_batched = get_data(C_eval, batch_size=batch_size)
+test_x_batched, test_y_batched = get_data(C_test, batch_size=batch_size)
+# Constants
+vocab_size = V_train.vocab_size  # get true vocab size
+time_steps = sentence_len - 1
+
+# Tensorboard constants
+timestamp = str(int(time()))
+out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+train_summary_dir = os.path.join(out_dir, "summaries", "train")
+
+# Models
+if task == "1a":
+    with tf.Graph().as_default():  # create graph for Experiment A
+        with tf.Session() as sess:
+            print("\nRunning Experiment A ...")
+            # input("Press Enter to continue...")
+            modelA = LSTM(V_train, embedding_size=100, hidden_size=512, time_steps=time_steps, clip_norm=clip_grad_norm)
+            train_model(modelA, sess, num_epochs, train_x_batched, train_y_batched, eval_x_batched, eval_y_batched,
+                        model_ckpt_name="modelA.ckpt", eval_every=eval_every)
+            perp = get_perplexity(modelA, sess, test_x_batched, test_y_batched,
+                                  V_train)  # compute perplexities on test set
+            write_out(perp, "group17.perplexityA")
+elif task == "1b":
+    with tf.Graph().as_default():  # create graph for Experiment B
+        with tf.Session() as sess:
+            print("\nRunning Experiment B ...")
+            # input("Press Enter to continue...")
+            modelB = LSTM(V_train, embedding_size=100, hidden_size=512, time_steps=time_steps, clip_norm=clip_grad_norm,
+                          load_external_embedding=True, embedding_path=embedding_path)
+            train_model(modelB, sess, num_epochs, train_x_batched, train_y_batched, eval_x_batched, eval_y_batched,
+                        model_ckpt_name="modelB.ckpt", eval_every=eval_every)
+            perp = get_perplexity(modelB, sess, test_x_batched, test_y_batched,
+                                  V_train)  # compute perplexities on test set
+            write_out(perp, "group17.perplexityB")
+elif task == "1c":
+    with tf.Graph().as_default():  # create graph for Experiment C
+        with tf.Session() as sess:
+            print("\nRunning Experiment C ...")
+            # input("Press Enter to continue...")
+            modelC = LSTM(V_train, embedding_size=100, hidden_size=1024, time_steps=time_steps,
+                          clip_norm=clip_grad_norm, down_project=True, down_projection_size=512)
+            train_model(modelC, sess, num_epochs, train_x_batched, train_y_batched, eval_x_batched, eval_y_batched,
+                        model_ckpt_name="modelC.ckpt", eval_every=eval_every)
+            perp = get_perplexity(modelC, sess, test_x_batched, test_y_batched,
+                                  V_train)  # compute perplexities on test set
+            write_out(perp, "group17.perplexityC")
+elif task == "2":
+    with tf.Graph().as_default():  # create graph for task 2
+        print("\nRunning Task 2 ...")
+        # Read sentences to complete
+        sentences = []
+        with open(prompt_path) as sentences_file:
+            for sentence in sentences_file:
+                tokens = sentence.strip().split(" ")
+                sentences.append(tokens)
+
+        # Pick the model to use
+        models_dir = os.path.join(os.getcwd(), "models")
+        model_path = os.path.join(models_dir, model_task2)
+        if model_task2 == "modelA.ckpt":
+            model2 = LSTM(V_train, embedding_size=100, hidden_size=512, time_steps=time_steps, clip_norm=clip_grad_norm)
+        elif model_task2 == "modelB.ckpt":
+            model2 = LSTM(V_train, embedding_size=100, hidden_size=512, time_steps=time_steps, clip_norm=clip_grad_norm,
+                          load_external_embedding=True, embedding_path=embedding_path)
+        elif model_task2 == "modelC.ckpt":
+            model2 = LSTM(V_train, embedding_size=100, hidden_size=1024, time_steps=time_steps,
+                          clip_norm=clip_grad_norm, down_project=True, down_projection_size=512)
+
+        with tf.Session() as sess:
+            # Load model
+            if not os.path.exists(models_dir) or not model2.load_model(sess, model_path):  # if couldn't load model
+                print("Please train the model first.")
+                sys.exit(-1)
+
+            # Complete sentences
+            model2.build_sentence_completion_graph()
+            print("\nSentence Completion Graph Built")
+            completed_sentences = []
+            nb_completed = 0
+            for sentence in sentences:  # for each tokenized sentence
+                prompt = V_train.get_tokens_ids(sentence)
+                continuation = model2.sentence_continuation(sess, prompt, V_train, max_length)
+                nb_completed += 1
+                if nb_completed == 1 or nb_completed % print_every == 0:
+                    print("Completed {} sentence".format(nb_completed))
+                completed_sentences.append(sentence + continuation)
+
+        # Write out completed sentences
+        write_path = os.path.join(os.getcwd(), "output", "group17.continuation")
+        print("\nWriting Completed Sentences")
+        with open(write_path, "w") as file:
+            for sentence in completed_sentences:
+                for word in sentence:
+                    if word == V_train.PAD_token or word == V_train.BOS_token:
+                        continue
+                    file.write(word + ' ')
+                file.write('\n')
+else:
+    raise ValueError("Wut unrecognized task {}".format(task))
---
+++ project1/model.py
@@ -0,0 +1,209 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.rnn import LSTMCell as Cell
+
+from load_embedding import load_embedding
+
+
+class LSTM:
+    def __init__(self, V, embedding_size, hidden_size, time_steps, clip_norm,
+                 down_project=False, down_projection_size=None,
+                 load_external_embedding=False, embedding_path=None):
+        vocab_size = V.vocab_size
+        self.dtype = tf.float32
+        initializer = tf.contrib.layers.xavier_initializer(dtype=self.dtype)
+
+        self.hidden_size = hidden_size
+        self.vocab_size = V.vocab_size
+        self.time_steps = time_steps
+        self.down_project = down_project
+
+        # Model Variables
+        if down_project:
+            # projection matrix
+            self.W_p = tf.Variable(initializer((hidden_size, down_projection_size)), name="W_p", dtype=self.dtype)
+            # projection biases
+            self.biases_p = tf.Variable(initializer([down_projection_size]), name="biases_p", dtype=self.dtype)
+            # weights
+            self.W = tf.Variable(initializer((down_projection_size, vocab_size)), name="W", dtype=self.dtype)
+        else:
+            self.W = tf.Variable(initializer((hidden_size, vocab_size)), name="W", dtype=self.dtype)  # weights
+
+        self.biases = tf.Variable(initializer([vocab_size]), name="biases", dtype=self.dtype)
+
+        if load_external_embedding:  # we're loading the embedding matrix
+            matrix = load_embedding(V.token2id, embedding_path, embedding_size,
+                                    vocab_size)  # load embedding matrix from file
+            self.embedding_matrix = tf.Variable(matrix, name="embedding",
+                                                dtype=self.dtype)  # make it trainable too (further train embeddings)
+        else:  # not loading external embedding => make embedding_matrix a tf.Variable (i,e to be trainable)
+            self.embedding_matrix = tf.Variable(initializer((vocab_size, embedding_size)), name="embedding",
+                                                dtype=self.dtype)
+
+        # Create Model graph
+
+        # the input words of shape [batch_size, time_steps]
+        self.input_x = tf.placeholder(tf.int32, [None, time_steps])
+        # the target words of shape [batch_size, time_steps]
+        self.input_y = tf.placeholder(tf.int32, [None, time_steps])
+        # the embedded input of shape [batch_size, time_steps, embedding_size]
+        embedded_x = tf.nn.embedding_lookup(self.embedding_matrix, self.input_x)
+
+        # LSTM cell with hidden state of size hidden_size
+        self.rnn = Cell(num_units=hidden_size, initializer=initializer, name="cell")
+
+        # LSTM cell initial state
+        self.initial_state = state = self.rnn.zero_state(tf.shape(embedded_x)[0], dtype=self.dtype)
+
+        logits = None
+
+        for t in range(time_steps):
+            # input the slice t (i,e all embedded vectors of the batch at timestep t)
+            output, state = self.rnn(embedded_x[:, t], state)
+            if down_project:
+                # first project then compute logit
+                logit = tf.matmul(tf.matmul(output, self.W_p) + self.biases_p, self.W) + self.biases
+            else:
+                logit = tf.matmul(output, self.W) + self.biases
+            logit = tf.reshape(logit, (tf.shape(logit)[0], 1, tf.shape(logit)[1]))  # reshape for later concat
+
+            if logits is None:  # first output logit
+                logits = logit
+            else:  # concat to previous output logits
+                logits = tf.concat((logits, logit), axis=1)
+
+        self.logits = logits
+
+        self.final_state = state
+
+        # Optimizer and loss
+        self.global_step = tf.Variable(0, name="global_step", trainable=False)
+        self.optimizer = tf.train.AdamOptimizer()
+        self.loss = self.compute_loss(logits=self.logits, labels=self.input_y)
+        self.clip_norm = clip_norm
+        self.train_op = self.get_train_op()  # training operation with clipped gradients
+
+        self.trainable_variables = [self.W, self.biases, self.embedding_matrix, self.rnn]
+        if down_project:
+            self.trainable_variables.append(self.W_p)
+        self.saver = tf.train.Saver()
+
+    def compute_loss(self, logits, labels):
+        # with tf.GradientTape() as tape:
+        self.losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+        return tf.reduce_sum(self.losses)
+
+    def get_train_op(self):  # perform gradient clipping and return train operation
+        grads_and_vars = self.optimizer.compute_gradients(
+            self.loss)  # compute gradient of loss function, returns list of tensor-variable pair
+        grads, vars = zip(*grads_and_vars)  # unzip
+        clipped_grads, _ = tf.clip_by_global_norm(grads, self.clip_norm)  # clip
+        return self.optimizer.apply_gradients(zip(clipped_grads, vars),
+                                              self.global_step)  # apply clipped gradients and return train op
+
+    def train_step(self, sess, input_words, output_words):
+        feed_dict = {self.input_x: input_words, self.input_y: output_words}
+        fetches = [self.train_op, self.global_step, self.loss]  # train and report loss
+
+        return sess.run(fetches, feed_dict)
+
+    def eval_step(self, sess, input_words, output_words):
+        feed_dict = {self.input_x: input_words, self.input_y: output_words}
+        fetches = [self.global_step, self.loss]  # only report loss
+
+        return sess.run(fetches, feed_dict)
+
+    def perplexity(self, sess, input_sentences, output_sentences, V):
+        losses_vals = sess.run(self.losses, feed_dict={self.input_x: input_sentences, self.input_y: output_sentences})
+        """
+        # Sanitation code to check self.losses        
+        probas = tf.nn.softmax(self.logits, axis=2)
+        probas_vals = sess.run(probas, feed_dict={self.input_x: input_sentences})
+        l = probas_vals.shape[0]
+        c = probas_vals.shape[1]
+        for i in range(l):
+            for j in range(c):
+                w = output_sentences[i, j]
+                p = probas_vals[i, j, w]
+                log_p_neg = -np.log(p) # nepierian logarithm
+                loss = losses_vals[i, j]
+                print("log_p_neg: {}, loss: {}, diff: {}".format(log_p_neg, loss, loss - log_p_neg))
+        """
+
+        s = output_sentences.shape[0]  # nb of sentences
+        perp = np.zeros(s)
+        pad_id = V.token2id[V.PAD_token]  # the id of the PAD token
+
+        # loop over output sentences i, we consider output sentences since <EOS> is used in perplexity computation while
+        # <BOS> isn't (since model shouldn't predict <BOS>)
+        for i in range(s):
+            # retrieve indices in the output sentence i where the token is not PAD
+            indices = np.where(output_sentences[i] != pad_id)
+            # compute the mean, only sum the losses vals of sentence i at the specified indices
+            exponent = np.mean(losses_vals[i][indices])
+            perp[i] = np.exp(exponent)  # use exp() since the losses are computed using Napierian logarithm ln()
+
+        return perp
+
+    def build_sentence_completion_graph(self):
+
+        self.c_in = tf.placeholder(tf.float32, shape=[1, self.rnn.state_size.c], name='c_in')
+        self.h_in = tf.placeholder(tf.float32, shape=[1, self.rnn.state_size.h], name='h_in')
+
+        self.int_state = tf.contrib.rnn.LSTMStateTuple(self.c_in, self.h_in)  # internal state
+        self.current_word = tf.placeholder(tf.int32, [1])
+        self.current_embed = tf.nn.embedding_lookup(self.embedding_matrix, self.current_word)
+
+        next_output, self.next_state = self.rnn(self.current_embed, self.int_state)
+
+        if self.down_project:
+            next_logit = tf.matmul(tf.matmul(next_output, self.W_p) + self.biases_p, self.W) + self.biases
+        else:
+            next_logit = tf.matmul(next_output, self.W) + self.biases
+
+        self.next_probabs = tf.nn.softmax(next_logit)
+
+    def sentence_continuation(self, sess, prompt, V, max_length):
+        # prompt: prefix to be completed
+        c_init = np.zeros((1, self.rnn.state_size.c), np.float32)
+        h_init = np.zeros((1, self.rnn.state_size.h), np.float32)
+        current_state = [c_init, h_init]
+        prompt_length = len(prompt)  # length of the prompt (without BOS)
+
+        prompt = [V.token2id[V.BOS_token]] + prompt  # extend prompt to add BOS token
+
+        for word in prompt:
+            current_c, current_h = current_state
+            feed_dict = {self.c_in: current_c, self.h_in: current_h, self.current_word: [word]}
+            next_probabs, current_state = sess.run([self.next_probabs, self.next_state], feed_dict)
+
+        current_word = np.argmax(next_probabs)
+
+        predicted_continuation = []
+
+        for i in range(max_length - prompt_length):
+            predicted_continuation.append(V.id2token[current_word])
+
+            current_c, current_h = current_state
+
+            feed_dict = {self.c_in: current_c, self.h_in: current_h, self.current_word: [current_word]}
+
+            next_probabs, current_state = sess.run([self.next_probabs, self.next_state], feed_dict)
+            current_word = np.argmax(next_probabs)
+            if current_word == V.token2id[V.EOS_token]:
+                break
+
+        return predicted_continuation
+
+    def save_model(self, sess, path):
+        self.saver.save(sess, path)
+        print("Model saved at %s" % path)
+
+    def load_model(self, sess, path):
+        try:  # try to load the model
+            self.saver.restore(sess, path)
+            print("Model restored from %s" % path)
+            return True
+        except:
+            print("Couldn't restore model")
+            return False
