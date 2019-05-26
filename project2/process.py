bed8571d661f8807ac4f58ee007b9b53994eb73c
ali-h <sonorligh@gmail.com>
2019-05-26 15:32:58 +0200

    Added bert model

--- project2/process.py
+++ project2/process.py
@@ -4,9 +4,12 @@ import numpy as np
 from tensorflow.keras.preprocessing import sequence
 
 
-def concat_sentences(df):
-    return reduce(lambda x, y: x.astype(str) + ' ' + y.astype(str),
-                  [df['sentence' + str(col)] for col in range(1, 6)]).values
+def concat_sentences(df, sentences=range(1, 6), sep=' '):
+    if isinstance(sentences, int):
+        return df['sentence' + str(sentences)].values
+    else:
+        return reduce(lambda x, y: x.astype(str) + sep + y.astype(str),
+                      [df['sentence' + str(col)] for col in sentences]).values
 
 
 def process(df, tokenizer, max_seq_len):
